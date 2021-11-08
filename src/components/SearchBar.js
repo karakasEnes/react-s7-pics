1c46f063d28a02708e953069e87cc5da3b38d88a
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-08 13:29:15 +0300

    handle input events, click, onchange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,30 @@
 import React from "react";
 
-const SearchBar = () => {
-  return (
-    <div className="ui segment">
-      <form className="ui form">
-        <div className="field">
-          <label>Image Search</label>
-          <input type="text" />
-        </div>
-      </form>
-    </div>
-  );
-};
+class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  handleInputClick(e) {
+    console.log(e.target.value);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              onChange={this.onInputChange}
+              onClick={this.handleInputClick}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
