6a2f8a16e2b7e084fcd0880e7ac3d310430055c1
Enes Karakaş <enes.karakas.e@gamil.com>
2021-11-09 13:51:56 +0300

    fix imagelist key=id problem

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ImageList = (props) => {
 
-    const imagesUrl = props.images.map(image => <img src={image.urls.regular} /> )
+    const imagesUrl = props.images.map(({id, description, urls}) => <img key={id} alt={description} src={urls.regular} /> )
     return <div>{imagesUrl}</div>
 
 }
